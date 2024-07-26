62cd66688bb5c518857a8d9fa592f04635ebc715
Yoon Hyeok <127106312+namyoonhyeok@users.noreply.github.com>
2024-07-26 17:28:51 +0900

    Add files via upload

---
+++ BE/plsbuymethis/src/main/java/com/example/plsbuymethis/PlsbuymethisApplication.java
@@ -0,0 +1,13 @@
+package com.example.plsbuymethis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlsbuymethisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlsbuymethisApplication.class, args);
+	}
+
+}
---
+++ BE/plsbuymethis/src/test/java/com/example/plsbuymethis/PlsbuymethisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plsbuymethis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlsbuymethisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
